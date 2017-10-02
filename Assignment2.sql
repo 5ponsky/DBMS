47ded270053d230717eca126ba101d074b567c45
Liam Dillingham <mailgd9@gmail.com>
2017-10-02 12:49:00 -0500

    finished

--- Assignment2.sql
+++ Assignment2.sql
@@ -5,4 +5,8 @@ SELECT COUNT(*) FROM Class WHERE (Enrollment > 100) AND (Room = 0);
 
 SELECT Room_No, Bldg FROM Rooms WHERE Capacity < (SELECT AVG(Capacity) FROM Rooms);
 
-SELECT Name FROM Building WHERE (SELECT * FROM Rooms WHERE Capacity > 50);
+SELECT DISTINCT Name FROM Building WHERE Code IN (SELECT DISTINCT Bldg FROM Rooms WHERE Capacity > 50);
+
+SELECT Name FROM Building WHERE Rooms = (SELECT MIN(Rooms) FROM (SELECT * FROM Building WHERE Rooms NOT IN (SELECT MIN(Rooms) FROM BUILDING)));
+
+SELECT Name FROM Dept WHERE Bldg IN (SELECT Code FROM Building WHERE Rooms = (SELECT MAX(Rooms) FROM Building));
