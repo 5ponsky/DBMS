57ff9395169eb2f99b7546ad1ae4fb64e39ca363
Liam Dillingham <mailgd9@gmail.com>
2017-10-20 13:42:48 -0500

    working on assignment3

--- Assignment3.sql
+++ Assignment3.sql
@@ -3,4 +3,10 @@
 
 SELECT SUM(C.Enrollment) FROM Class C GROUP BY Dept;
 
-SELECT B.Name, COUNT(R.Rooms) FROM Rooms R, Building B WHERE GROUP BY B.Name;
+SELECT Name FROM Building JOIN 
+
+
+SELECT B.Name, Count() FROM Building B WHERE B.Code IN
+    (SELECT Bldg FROM Class C, Dept D WHERE (C.Assigned IS NOT NULL) AND (D.Code = C.Dept))  GROUP BY Name;
+
+SELECT Dept FROM Class WHERE Assigned IS NOT NULL;
