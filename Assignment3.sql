ea10df5d306f73d6de878207f7631b37c65ac2a8
Liam Dillingham <mailgd9@gmail.com>
2017-10-25 16:04:49 -0500

    fuck me

--- Assignment3.sql
+++ Assignment3.sql
@@ -10,8 +10,8 @@ WHERE (Room IS NOT NULL AND Assigned IS NOT NULL) AND ()
 
 -- Problem 3
 SELECT Name
-FROM Class JOIN Dept ON Class.Dept = Dept.Code
-WHERE Dept = (SELECT Dept, MAX(Dept) FROM Class WHERE Dept IN (SELECT MAX(Dept) FROM Class WHERE (Room IS NULL AND Assigned IS NULL) GROUP BY Dept) GROUP BY Dept);
+FROM Dept
+WHERE Code IN (SELECT Dept, MAX(Dept) FROM Class WHERE Dept IN (SELECT MAX(Dept) FROM Class WHERE (Room IS NULL AND Assigned IS NULL) GROUP BY Dept) GROUP BY Dept);
 
 SELECT Name
 FROM Class JOIN Dept ON Class.Dept = Dept.Code
@@ -28,6 +28,14 @@ SELECT Dept FROM Class WHERE Assigned IS NOT NULL;
 
 select Dept FROM Class WHERE Enrollment = (SELECT Min(Enrollment) FROM Class) GROUP BY Dept;
 
-SELECT MAX(Dept) FROM Class WHERE Dept IN (SELECT COUNT(Dept) FROM Class) AND (Room IS NULL AND Assigned IS NULL);
+Select Name FROM Dept WHERE Code IN ( SELECT MAX(Dept) FROM Class C JOIN (SELECT COUNT(Dept) From Class WHERE (Room IS NULL AND Assigned IS NULL) GROUP BY Dept) on C.Dept = Class.Dept); -- removes dupes
 
-SELECT MAX()
+SELECT COUNT(Dept) AS num FROM Class WHERE (Room IS NULL AND Assigned IS NULL)
+
+SELECT Name
+FROM Dept
+WHERE Code IN (SELECT Dept, MAX(classCount.num) FROM (SELECT Dept, COUNT(Dept) AS num FROM Class WHERE (Room IS NULL AND Assigned IS NULL) GROUP BY Dept)classCount);
+
+SELECT * FROM (SELECT Dept, COUNT(Dept) AS num FROM Class WHERE (Room IS NULL AND Assigned IS NULL) GROUP BY Dept)classCount
+
+-- Problem 4
