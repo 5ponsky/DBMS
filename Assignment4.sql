e61ec55f1ea7927b4febaf46463f2dceae742532
Liam Dillingham <mailgd9@gmail.com>
2017-11-14 12:16:40 -0600

    ok

--- Assignment4.sql
+++ Assignment4.sql
@@ -5,4 +5,11 @@ SELECT Dept, Course_No, Phone FROM Class JOIN Dept ON Class.Dept = Dept.Code WHE
 SELECT DISTINCT Name, Contact, TO_CHAR(Assigned, 'DD') FROM Class JOIN Dept ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) BETWEEN (TO_DATE(SYSDATE) - 7) AND (TO_DATE(SYSDATE));
 
 -- Problem 3
-SELECT Room_No, Name, TO_CHAR(Assigned, 'DD'), DECODE(AV, 'Y', 'Yes', 'N', 'No') FROM Rooms JOIN Dept ON Rooms.Bldg = Dept.Bldg JOIN Class ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) LIKE '%%-OCT-17';
+SELECT Room_No, Name, TO_CHAR(Assigned, 'DD'), DECODE(AV, 'Y ', 'Yes', 'N ', 'No') FROM Rooms JOIN Dept ON Rooms.Bldg = Dept.Bldg JOIN Class ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) LIKE '%%-OCT-17';
+
+-- Problem 4
+SELECT Name, COUNT(Assigned) FROM Building FULL OUTER JOIN Rooms ON Building.Code = Rooms.Bldg FULL OUTER JOIN Class ON Rooms.Room_No = Class.Room WHERE Building.Code IN (SELECT Bldg FROM Rooms JOIN Class ON Rooms.Room_No = Class.Room WHERE Assigned IS NOT NULL) GROUP BY Building.Name;
+
+SELECT * FROM Rooms JOIN Class ON Rooms.Room_No = Class.Room WHERE Assigned IS NOT NULL;
+
+, COUNT(*) 
