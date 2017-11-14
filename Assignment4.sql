b8f304fab29f3745a22ffc4ba2c0d991c64631e5
Liam Dillingham <mailgd9@gmail.com>
2017-11-14 10:39:45 -0600

    dfg

---
+++ Assignment4.sql
@@ -0,0 +1,8 @@
+-- Problem 1
+SELECT Dept, Course_No, Phone FROM Class JOIN Dept ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) > TO_DATE(SYSDATE);
+
+-- Problem 2
+SELECT DISTINCT Name, Contact, TO_CHAR(Assigned, 'DD') FROM Class JOIN Dept ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) BETWEEN (TO_DATE(SYSDATE) - 7) AND (TO_DATE(SYSDATE));
+
+-- Problem 3
+SELECT Room_No, Name, TO_CHAR(Assigned, 'DD'), DECODE(AV, 'Y', 'Yes', 'N', 'No') FROM Rooms JOIN Dept ON Rooms.Bldg = Dept.Bldg JOIN Class ON Class.Dept = Dept.Code WHERE TO_DATE(Assigned) LIKE '%%-OCT-17';
